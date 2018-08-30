caf07b06778aef2b73adf2a88920117fcd1621f9
bymmry <bymmry@gmail.com>
2018-08-31 07:55:04 +0800

    stock

--- server/app/field/Stock.js
+++ server/app/field/Stock.js
@@ -2,10 +2,14 @@ module.exports = {
   type: {
     name: '库存变化类型',
     type: 'String',
-    option: {
+    option: { 
       in: '入库',
       out: '出库',
-      check: '盘点'
+      check: '盘点',
+      increase: '增益',
+      decrease: '损耗',
+      checkIncrease: '盘点增益',
+      checkDecrease: '盘点损耗',
     }
   },
   name: {
@@ -15,5 +19,9 @@ module.exports = {
   num: {
     name: '数量',
     type: 'Number',
+  },
+  company: {
+    name: '所属公司',
+    type: 'ObjectId'
   }
 }
