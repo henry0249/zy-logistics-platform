3bbcea5ded15cb35913485f6dc5e93259b2d9b2b
hzy1996 <hzy199060820@gmail.com>
2018-10-22 19:22:18 +0800

    refactor(/sys/company/CompanyEditItem): change commonMultiSelect
    替换贸易链关联公司和物流链关联公司

--- cms/src/views/goods/Test.vue
+++ cms/src/views/goods/Test.vue
@@ -3,9 +3,9 @@
     <!-- <div style="width:200px;height:100px;background:#369;margin:100px" @click="$router.push({path:'/stock/ready_list/edit?type=out&_id=5bc0183c1e3bf2cd9f7b232e'})">
       11111111111111111
     </div>
-    <input type="button" name="anniu2" @click='copyUrl' value="复制URL地址">
-    <i v-copy="'hahahhaa'">hahahhaa</i> -->
-    <common-select-by-code size="mini" label="啦啦啦啦啦啦啦啦啦啦"></common-select-by-code>
+    <input type="button" name="anniu2" @click='copyUrl' value="复制URL地址"> -->
+    <i v-copy="'hahahhaa'">hahahhaa</i>
+    <!-- <common-select-by-code size="mini" label="啦啦啦啦啦啦啦啦啦啦"></common-select-by-code> -->
   </div>
 </template>
 
